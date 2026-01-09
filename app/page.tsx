af93844488314593be5578ed6b86e214b79301a8
chan-web-source <chan.web.work@gmail.com>
2026-01-09 18:43:30 +0800

    moved positioning

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { LiveAvatarSection } from "../src/components/steraming-section";
+import { LiveAvatarSection } from "./components/steraming-section";
 
 export default function Home() {
   return <LiveAvatarSection />;
